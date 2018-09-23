c4f3ba78eae0a752ea546a374f88efdc27a673fe
chanstal <43198566+chanstal@users.noreply.github.com>
2018-09-24 00:51:57 +0800

    Add files via upload

---
+++ 4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+     float v1 = scn.nextInt();
+     float v2 =scn.nextInt();
+        System.out.println(v1/2.54);
+        System.out.println(v2/0.454);
+
+    }
+}
