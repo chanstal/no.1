e40f57febbe7d0173a3713dfce47ff8fef349ed9
chanstal <43198566+chanstal@users.noreply.github.com>
2018-09-24 00:34:34 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,9 +5,9 @@ public class Main {
         Scanner scn = new Scanner(System.in);
         int v1 = scn.nextInt();
         int v2 =scn.nextInt();
-        int v3 =scn.nextInt();
-        System.out.println(v1+v2+v3);
-        System.out.println((v1+v2+v3)/(float)3);
-
-    }
+        System.out.println(v1+v2);
+        System.out.println(v1-v2);
+        System.out.println(v1*v2);
+        System.out.println(v1/(float)v2);
+}
 }
